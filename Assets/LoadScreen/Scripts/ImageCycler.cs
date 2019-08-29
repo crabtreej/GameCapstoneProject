056c74104892e9aa66ae807ec80d21cfb908d980
Jacob Crabtree <crabtree.234@osu.edu>
2019-08-29 17:57:34 -0400

    Wrote code for Image Cycler, added more to .gitignore.

--- Assets/LoadScreen/Scripts/ImageCycler.cs
+++ Assets/LoadScreen/Scripts/ImageCycler.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ImageCycler : MonoBehaviour
 {
+    public UnityEngine.UI.Image image;
+    public List<Sprite> imagesToShow;
+    public float timeBetweenUpdates;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(UpdateImageCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator UpdateImageCoroutine()
     {
-        
+        WaitForSeconds wait = new WaitForSeconds(timeBetweenUpdates);
+        int currentImage = 0;
+        while(true)
+        {
+            image.sprite = imagesToShow[currentImage];
+            currentImage = (currentImage + 1) % imagesToShow.Count;
+            yield return wait;
+        }
     }
 }
