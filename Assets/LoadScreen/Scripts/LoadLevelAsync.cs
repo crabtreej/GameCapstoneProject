888339997c39f195624bfff8813aa124631e1c03
Jacob Crabtree <crabtree.234@osu.edu>
2019-08-29 22:21:08 -0400

    Added script to load a level async and report progress, and a progress bar to reflect.

---
+++ Assets/LoadScreen/Scripts/LoadLevelAsync.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevelAsync : MonoBehaviour
+{
+
+    public float progress { get; private set; }
+    
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        var sceneLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("ToonMaze");
+        sceneLoad.allowSceneActivation = false;
+
+        while(!sceneLoad.isDone && sceneLoad.progress < 0.89)
+        {
+            progress = sceneLoad.progress;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3f);
+        sceneLoad.allowSceneActivation = true;
+        progress = 1f;
+    }
+}
---
+++ Assets/LoadScreen/Scripts/ShowProgress.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowProgress : MonoBehaviour
+{
+    public LoadLevelAsync levelLoader;
+
+    private Image progressBar;
+    void Start()
+    {
+        progressBar = GetComponent<Image>();
+        progressBar.fillMethod = Image.FillMethod.Horizontal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressBar.fillAmount = levelLoader.progress;
+    }
+}
