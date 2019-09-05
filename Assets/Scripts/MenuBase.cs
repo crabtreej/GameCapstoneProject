8c8d6c96654db071396fcee2ed0509867320bc9e
Joshua Jacobs <Joshua9788@hotmail.com>
2019-09-05 15:33:45 -0400

    Added pause menu

---
+++ Assets/Scripts/MenuBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBase : MonoBehaviour {
+
+    public Selectable selectOnEnable;
+
+
+    private void OnEnable()
+    {
+        if (Input.GetJoystickNames().Length > 0)
+        {
+            selectOnEnable.Select();
+        }
+    }
+
+
+}
