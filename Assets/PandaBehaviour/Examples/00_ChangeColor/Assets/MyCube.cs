f0576bb239b4569faf366c2c4fc9075e58158cba
Jacob Crabtree <crabtree.234@osu.edu>
2019-09-07 19:28:22 -0400

    Added in a BehaviorTrees library and got the original behavior of the AI working plus he explores the last known location of the player.

---
+++ Assets/PandaBehaviour/Examples/00_ChangeColor/Assets/MyCube.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Panda;
+
+namespace Panda.Examples.ChangeColor
+{
+    public class MyCube : MonoBehaviour
+    {
+
+        int ctr;
+        /*
+         * Set the color to the specified rgb value and succeed.
+         */
+        [Task] // <-- Attribute used to tag a class member as a task implementation.
+        void SetColor(float r, float g, float b)
+        {
+            this.GetComponent<Renderer>().material.color = new Color(r, g, b);
+            Task.current.Succeed(); // <-- Task.current gives access to the run-time task bind to this method.
+        }
+
+        [Task]
+        bool True = true;
+        void Failed()
+        {
+            Task.current.Fail();
+        }
+
+        [Task]
+        void SucceedTask()
+        {
+            Task.current.Succeed();
+        }
+
+        [Task]
+        bool TrueLongTime()
+        {
+            ctr += 1;
+            if (ctr > 1000)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.AI;
+using Panda;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class AIController : MonoBehaviour
 {
     public GameObject player;
-    AIStateMachine machine;
+    private bool heardPlayer;
+    private float halfFOVRad;
+    private float fovDeg = 120f;
+    NavMeshAgent agent;
 
     /**
      * Start should be used to create any states that you want the StateMachine
@@ -12,18 +20,112 @@ public class AIController : MonoBehaviour
      */
     void Start()
     {
-        //Random.InitState((int)(Time.realtimeSinceStartup * 1000));
-        machine = new AIStateMachine(player, gameObject);
-        IState wanderState = new AIWanderState(machine);
-        IState chaseState = new AIChaseState(machine);
-        machine.SetInitialState(wanderState);
-        machine.AddTransition(wanderState, chaseState);
-		machine.AddTransition(chaseState, wanderState);
+        EventCenter.Instance.ObjectMadeNoise.AddListener(heardNoiseEventListener);
+        halfFOVRad = (((fovDeg / 2) * Mathf.PI) / 180f);
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // This gets fired by any objects that make noise (or at least, objects that
+    // make noise *should* fire this event).
+    private void heardNoiseEventListener(GameObject noiseSource)
     {
-        machine.Update();
+        Debug.Log("Heard a noise in Wander State.");
+        if (noiseSource == player)
+        {
+            
+            heardPlayer = true;
+        }
+    }
+
+    private IEnumerator ClearPlayerHeard()
+    {
+        yield return new WaitForSeconds(3f);
+        heardPlayer = false;
+    }
+
+    private bool canHearPlayer()
+    {
+        // If we've heard the player and they are fairly nearby, we'll say that we've actually heard the player
+        bool heard = false;
+        if(heardPlayer && (AIUtility.findWalkableDistance(transform.position, player.transform.position) <= 20f))
+        {
+            StopCoroutine(ClearPlayerHeard());
+            StartCoroutine(ClearPlayerHeard());
+            heard = true;
+            Debug.Log("Heard player");
+        }
+        
+        // Want to clear this so it isn't cheating by always hearing them
+        return heard;
+    }
+
+    // If the player is in an unobstructed line of sight to the AI and the AI is actually facing
+    // the player, AND the player is super super far away, then we can see the player
+    private bool canSeePlayer()
+    {
+        float distToPlayer = AIUtility.findWalkableDistance(transform.position, player.transform.position);
+        bool inFOV = AIUtility.objectInFieldOfView(player.transform.position, transform, halfFOVRad);
+        bool hasLineOfSight = AIUtility.objectVisibleFromPosition(player.transform.gameObject, transform.position);
+
+        return (distToPlayer <= 50f && inFOV && hasLineOfSight);
+    }
+
+   /**
+    * FoundPlayer, WanderRandomly, ChasePlayer
+    */
+    [Task]
+    bool FoundPlayer()
+    {
+        if (canSeePlayer() || canHearPlayer())
+        {
+            Debug.Log("Leaving AI Wander state!");
+            return true;
+        }
+        return false;   
+    }
+
+    [Task]
+    void WanderRandomly()
+    {
+        // This is size of the circle we draw around the player
+        int m_range = 20;
+        // If we're still heading to the last chosen 
+        // destination, then just quit out
+        if (!Task.current.isStarting && (agent.pathPending || agent.remainingDistance > 0.5f))
+            return;
+
+        // Essentially draw a circle around the player, then pick a random point
+        // in the circle, with radius 20, and send the AI to that X,Z coordinate
+        Vector2 rand = Random.insideUnitCircle;
+        agent.SetDestination(player.transform.position + m_range * new Vector3(rand.x, 0.2f, rand.y));
+        if(Task.isInspected)
+        {
+            Task.current.debugInfo = agent.destination.ToString();
+        }
+    }
+
+    [Task]
+    void ChasePlayer()
+    {
+        agent.SetDestination(player.transform.position);
+        if(AIUtility.findWalkableDistance(transform.position, player.transform.position) <= 1.2f)
+        {
+            Task.current.Succeed();
+        }
+    }
+
+    [Task]
+    void ExploreLastPlayerLocation()
+    {
+        if(!(agent.pathPending || agent.remainingDistance > 0.5f) || FoundPlayer())
+        {
+            Task.current.Succeed();
+        }
+    }
+
+    [Task]
+    void KillPlayer()
+    {
+        SceneManager.LoadScene("Exit");
     }
 }
